8e3610408b4bc19eba43b4844e9ae594ff1e445e
gerwyn <gerwynsim@hotmail.com>
2025-08-12 13:52:50 +0800

    prisma

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,19 @@
 "use client";
+import {ThemeProvider} from "./theme-provider";
 
-function providers() {
-  return <div>providers</div>;
+function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
 }
-export default providers;
+
+export default Providers;
