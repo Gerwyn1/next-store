4f485e7e88f7aaa44b2e47aad7c82863d82eb536
gerwyn <gerwynsim@hotmail.com>
2025-08-14 14:39:16 +0800

    form components

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,3 +1,4 @@
+import FormInput from "@/components/form/FormInput";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -19,8 +20,12 @@ function CreateProductPage() {
       <div className="border p-8 rounded-md">
         <form action={createProductAction}>
           <div className="mb-2">
-            <Label htmlFor="name">Product Name</Label>
-            <Input id="name" name="name" type="text" defaultValue={name} />
+            <FormInput
+              type="text"
+              name="name"
+              label="product name"
+              defaultValue={name}
+            />
           </div>
           <Button type="submit" size="lg">
             Submit
