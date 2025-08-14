c44e0354ea48786fc9620576bf8129c71b806a42
gerwyn <gerwynsim@hotmail.com>
2025-08-14 10:08:57 +0800

    restrict access to admin page

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -12,8 +12,11 @@ import { links } from "@/utils/links";
 import UserIcon from "./UserIcon";
 import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
 import SignOutLink from "./SignOutLink";
+import { auth } from "@clerk/nextjs/server";
 
 function LinksDropdown() {
+  const { userId } = auth();
+  const isAdmin = userId === process.env.ADMIN_USER_ID;
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -40,6 +43,7 @@ function LinksDropdown() {
         {/* IF SIGNED IN */}
         <SignedIn>
           {links.map((link) => {
+            if (link.label === "dashboard" && !isAdmin) return null;
             return (
               <DropdownMenuItem key={link.href}>
                 <Link href={link.href} className="capitalize w-full">
