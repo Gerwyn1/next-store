86418ae4e66670f418364e516746eedbf7ecf063
gerwyn <gerwynsim@hotmail.com>
2025-08-15 08:58:32 +0800

    image validation

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,7 +3,7 @@
 import db from "@/utils/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { productSchema, validateWithZodSchema } from "./schemas";
+import { imageSchema, productSchema, validateWithZodSchema } from "./schemas";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -58,6 +58,7 @@ export const createProductAction = async (
   const user = await getAuthUser();
 
   try {
+    const file = formData.get("image") as File;
     const rawData = Object.fromEntries(formData);
 
     // (generic error message)
@@ -72,6 +73,7 @@ export const createProductAction = async (
         clerkId: user.id,
       },
     });
+    const validateFile = validateWithZodSchema(imageSchema, { image: file });
 
     // const validatedFields = productSchema.safeParse(rawData);
     // if (!validatedFields.success) {
