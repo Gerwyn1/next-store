0639420018c262dd53ab98e29121c94166af7fcd
gerwyn <gerwynsim@hotmail.com>
2025-08-16 09:42:33 +0800

    favorite buttons and actions

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -71,3 +71,41 @@ export const IconButton = ({ actionType }: { actionType: actionType }) => {
     </Button>
   );
 };
+
+const CardSignInButton = () => {
+  return (
+    <SignInButton mode="modal">
+      <Button
+        type="button"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
+  );
+};
+
+const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+  return (
+    <SignInButton mode="modal">
+      <Button
+        type="submit"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+      >
+        {pending ? (
+          <ReloadIcon className="animate-spin" />
+        ) : isFavorite ? (
+          <FaHeart />
+        ) : (
+          <FaRegHeart />
+        )}
+      </Button>
+    </SignInButton>
+  );
+};
