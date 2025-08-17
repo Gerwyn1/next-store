db058db350c4e8f683a4d53d0af14d6c995e4065
gerwyn <gerwynsim@hotmail.com>
2025-08-17 16:11:53 +0800

    product rating

--- utils/actions.ts
+++ utils/actions.ts
@@ -313,6 +313,25 @@ export const fetchProductReviews = async (productId: string) => {
   return reviews;
 };
 
+export const fetchProductRating = async (productId: string) => {
+  const result = await db.review.groupBy({
+    by: "productId",
+    _avg: {
+      rating: true,
+    },
+    _count: {
+      rating: true,
+    },
+    where: {
+      productId,
+    },
+  });
+  return {
+    rating: result[0]?._avg.rating?.toFixed(1) ?? 0,
+    count: result[0]?._count.rating ?? 0,
+  };
+};
+
 export const fetchProductReviewByUser = async () => {
   // return db.review.findMany({});
 };
@@ -322,9 +341,5 @@ export const deleteReviewAction = async () => {
 };
 
 export const findExistingReview = async () => {
-  // return db.review.findMany({});
-};
-
-export const fetchProductRating = async () => {
   // return db.review.findMany({});
 };
