fdf941af6f4b32681a76c0840da6dec6c8a6ebb1
gerwyn <gerwynsim@hotmail.com>
2025-08-19 13:18:28 +0800

    test stripe function

--- utils/actions.ts
+++ utils/actions.ts
@@ -628,7 +628,7 @@ export const createOrderAction = async (prevState: any, formData: FormData) => {
   } catch (error) {
     return renderError(error);
   }
-  redirect(`/checkout?orderId${orderId}&cartId=${cartId}`);
+  redirect(`/checkout?orderId=${orderId}&cartId=${cartId}`);
 };
 
 export const fetchUserOrders = async () => {
